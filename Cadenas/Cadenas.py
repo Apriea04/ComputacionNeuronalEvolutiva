a44a7dbeeaf77eb840b652f1bacfdb8903f119ba
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-02-22 21:09:01 +0100

    Agregado archivo .gitignore y código para operadores en cadenas. Hecha práctica

--- Cadenas/Cadenas.py
+++ Cadenas/Cadenas.py
@@ -1,67 +1,40 @@
-import random
-import matplotlib.pyplot as plt
+from Genética_1000_unos import mutar, crossover, aptitud, seleccionar_ruleta
+import numpy as np
 
-N = 10
+INDIVIDUOS = 100
+GENES = 100
+MUTACION = 0.1
 
-def seleccion_ruleta(poblacion, n):
-    """Selecciona n individuos de la población usando el método de la ruleta"""
-    aptitudes = [aptitud(individuo) for individuo in poblacion]
-    poblacion_seleccionada = []
-    aptitudes_acumuladas = [sum(aptitudes[: i + 1]) for i in range(len(aptitudes))]
-    aptitud_total = aptitudes_acumuladas[-1]
-    for _ in range(n):
-        valor = random.randint(0, aptitud_total)
-        
-        seleccionado = 0
-        
-        while not aptitudes_acumuladas[seleccionado] >= valor:
-                seleccionado += 1
-        poblacion_seleccionada.append(poblacion[seleccionado])
-    return poblacion_seleccionada
+#Crear una poblacion de 12 individuos con 100 genes conteniendo 0s y 1s
+poblacion = np.random.choice([0,1], size=(INDIVIDUOS, GENES))
 
-def crossover_simple(progenitores):
-    """Realiza el crossover simple entre dos individuos. Devuelve el hijo resultante."""
-    indice_corte = random.randint(0, len(progenitores[0]) - 1)
-    return progenitores[0][:indice_corte] + progenitores[1][indice_corte:]
-
-def crear_poblacion_cadenas(n, longitud):
-    """Crea una población de n individuos, cada uno de longitud longitud."""
-    poblacion = []
-    for i in range(n):
-        poblacion.append([])
-        for _ in range(longitud):
-            poblacion[i].append(random.choice([0, 1]))
-    return poblacion
-
-def aptitud(individuo):
-    """Calcula la aptitud de un individuo, que es la suma de sus valores."""
-    return sum([int(x) for x in individuo])
-
-def mutar(individuo):
-    """Muta un individuo, cambiando uno de sus valores aleatoriamente."""
-    indice = random.randint(0, len(individuo) - 1)
-    nuevo_valor = 1 if individuo[indice] == 0 else 0
-    individuo[indice] = nuevo_valor
-    return individuo
-
-poblacion = crear_poblacion_cadenas(N, 100)
 mejores_aptitudes = []
 aptitud_promedio = []
-for _ in range(1000):
-    aptitud_promedio.append(sum([aptitud(individuo) for individuo in poblacion]) / N)
-    mejores_aptitudes.append(max([aptitud(individuo) for individuo in poblacion]))
-    #Creamos una nueva generación
-    nueva_generacion = []
-    for _ in range(N):
-        padres = seleccion_ruleta(poblacion, 2)
-        hijo = crossover_simple(padres)
-        #Mutamos el hijo con una probabilidad del 1%
-        if random.random() < 0.01:
-            hijo = mutar(hijo)
-        nueva_generacion.append(hijo)
-    poblacion = nueva_generacion
+mejor_aptitud_actual = 0
 
-#Dibujamos la gráfica del funcionamiento
+max_iteraciones = 5000
+
+while mejor_aptitud_actual < 100 and len(mejores_aptitudes) < max_iteraciones:
+    nueva_generacion = []
+    while len(nueva_generacion) < INDIVIDUOS:
+        padres = seleccionar_ruleta(poblacion, 2)
+        nuevo_individuo = crossover(padres)
+        nueva_generacion.append(nuevo_individuo)
+    nueva_generacion = np.array(nueva_generacion)
+    mutar(nueva_generacion, MUTACION)
+    aptitudes = [aptitud(individuo) for individuo in nueva_generacion]
+    mejor_aptitud_actual = np.max(aptitudes)
+    mejores_aptitudes.append(mejor_aptitud_actual)
+    aptitud_promedio.append(np.mean(aptitudes))
+    poblacion = nueva_generacion
+    
+    if len(mejores_aptitudes) % 100 == 0:
+        print(f'Generación {len(mejores_aptitudes)} Mejor aptitud {mejor_aptitud_actual}, aptitud promedio {aptitud_promedio[-1]}')
+
+print(mejores_aptitudes[-1])
+
+# Usar matplotlib para graficar la aptitud promedio y la mejor aptitud
+import matplotlib.pyplot as plt
 plt.plot(aptitud_promedio, label='Aptitud promedio')
 plt.plot(mejores_aptitudes, label='Mejor aptitud')
 plt.xlabel('Generación')
--- Cadenas/Genética_1000_unos.py
+++ Cadenas/Genética_1000_unos.py
@@ -100,6 +100,7 @@ def crossover(
     tipo: int = 1,
     num_progenitores_involucrados: int = 2,
     verbose: bool = False,
+    probabilidad: float = 1,
 ) -> np.ndarray:
     """Produce un nuevo individuo a partir de algún tipo de crossover, involucrando a
     num_progenitores_involucrados de entre *progenitores.
@@ -111,51 +112,57 @@ def crossover(
     :return: matriz de individuos con el nuevo individuo agregado"""
 
     match tipo:
-        # 1: Corte sencillo
+        # 1: Hijo único "biológico natural"
         case 1:
-            indices_corte = [
-                random.randint(0, len(progenitores[0]) - 1)
-                for _ in range(num_progenitores_involucrados - 1)
-            ]
-            if verbose:
-                print("Índices de corte:", indices_corte)
-            # Obtengo solo a los progenitores involucrados entre los disponibles
-            progenitores_involucrados = random.sample(
-                list(progenitores), num_progenitores_involucrados
-            )
-            if verbose:
-                print("Progenitores involucrados:", progenitores_involucrados)
-            # Declaro al sucesor
-            sucesor = []
-            progenitor_actual = 0
-            for i in range(len(progenitores[0])):
-                sucesor.append(progenitores_involucrados[progenitor_actual][i])
-                if i in indices_corte:
-                    progenitor_actual += 1
-            if verbose:
-                print("Sucesor:", sucesor)
+            if random.uniform(0, 1) <= probabilidad:
+                indices_corte = [
+                    random.randint(0, len(progenitores[0]) - 1)
+                    for _ in range(num_progenitores_involucrados - 1)
+                ]
+                if verbose:
+                    print("Índices de corte:", indices_corte)
+                # Obtengo solo a los progenitores involucrados entre los disponibles
+                progenitores_involucrados = random.sample(
+                    list(progenitores), num_progenitores_involucrados
+                )
+                if verbose:
+                    print("Progenitores involucrados:", progenitores_involucrados)
+                # Declaro al sucesor
+                sucesor = []
+                progenitor_actual = 0
+                for i in range(len(progenitores[0])):
+                    sucesor.append(progenitores_involucrados[progenitor_actual][i])
+                    if i in indices_corte:
+                        progenitor_actual += 1
+                if verbose:
+                    print("Sucesor:", sucesor)
 
-            return np.array(sucesor)
+                return np.array(sucesor)
+            else:
+                # Si no se realiza el crossover, devuelvo un único progenitor aleatoio
+                return np.array(random.choice(list(progenitores)))
+        # 2: Corte sencillo. Dos padres, dos hijos
+        case 2:
+            if random.uniform(0, 1) <= probabilidad:
+                # Me quedo con dos progenitores al azar
+                progenitores = random.sample(list(progenitores), 2)
+                # Elijo un punto de corte
+                punto_corte = random.randint(1, len(progenitores[0]) - 1)
+                # Creo a los hijos
+                hijo1 = np.concatenate((progenitores[0][:punto_corte], progenitores[1][punto_corte:]))
+                hijo2 = np.concatenate((progenitores[1][:punto_corte], progenitores[0][punto_corte:]))
+                # Los devuelvo como ndarray
+                return np.array([hijo1, hijo2])
+            else:
+                return np.array(progenitores)
 
 
-def run():
-    # Creo una población de 12 individuos
-    poblacion = crear_poblacion(12, 100)
+def seleccion_elitista(poblacion: np.ndarray, n: int) -> np.ndarray:
+    """Selecciona a los n individuos más aptos de la población.
+    :param poblacion: individuos de entre los que seleccionar
+    :param n: número de individuos a seleccionar
+    :return: individuos seleccionados"""
 
-    # Guardo la media inicial en medias
-    sumas = np.sum(poblacion, 1)
-    print(sumas)
-    medias = [np.mean(sumas)]
-    print(medias)
-    mejores = np.max(sumas)
-    print(mejores)
-
-    for _ in range(150):
-        nueva_generacion = []
-        for _ in range(12):
-            crossover(nueva_generacion, 1, 2, False, seleccionar_padres(poblacion, 2))
-        mutar(nueva_generacion, 0.01)
-        poblacion = nueva_generacion
-        sumas = np.sum(poblacion, 1)
-        medias.append(np.mean(sumas))
-        mejores = np.max(sumas)
+    aptitudes = [aptitud(individuo) for individuo in poblacion]
+    indices = np.argsort(aptitudes)
+    return poblacion[indices[-n:]]
