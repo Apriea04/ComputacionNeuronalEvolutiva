6d5b49d9b4d9aa10edf3739d9208f75141d9058c
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-03-12 21:22:07 +0100

    Agregada función para calcular distancias en base a diferentes métricas

--- Viajante/Datos/crear_matriz.py
+++ Viajante/Datos/crear_matriz.py
@@ -25,7 +25,7 @@ def crear_lista_coordenadas(size: int, path: str, min: float = -100, max: float 
             file.write(f"{coordenadas[i][0]} {coordenadas[i][1]}\n")
     return coordenadas
 
-def coordenadas_a_distancias(path_coordenadas: str, path_distancias: str):
+def coordenadas_a_distancias(path_coordenadas: str, path_distancias: str, distancia: str = "euclidea"):
     """Lee el fichero con las coordenadas y produce  la matriz de distancias correspondiente"""
     
     # Leer las coordenadas
@@ -38,10 +38,18 @@ def coordenadas_a_distancias(path_coordenadas: str, path_distancias: str):
     
     for i in range(size):
         for j in range(i + 1, size):
-            matriz[i][j] = np.sqrt((coordenadas[i][0] - coordenadas[j][0])**2 + (coordenadas[i][1] - coordenadas[j][1])**2)
+            match distancia:
+                case "euclidea":
+                    matriz[i][j] = np.sqrt((coordenadas[i][0] - coordenadas[j][0])**2 + (coordenadas[i][1] - coordenadas[j][1])**2)
+                case "manhattan":
+                    matriz[i][j] = abs(coordenadas[i][0] - coordenadas[j][0]) + abs(coordenadas[i][1] - coordenadas[j][1])
+                case "chebyshev":
+                    matriz[i][j] = max(abs(coordenadas[i][0] - coordenadas[j][0]), abs(coordenadas[i][1] - coordenadas[j][1]))
             matriz[j][i] = matriz[i][j]
     np.savetxt(path_distancias, matriz, fmt="%d")
     
 
 if __name__ == "__main__":
-    crear_lista_coordenadas(10, "Viajante/Datos/10k.data")
+    crear_lista_coordenadas(10, "Viajante/Datos/10_coordenadas.txt")
+    coordenadas_a_distancias("Viajante/Datos/10_coordenadas.txt", "Viajante/Datos/10_distancias.txt")
+        
