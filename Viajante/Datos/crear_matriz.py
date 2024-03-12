21822c2f0b47fe5962eb74d464c40797e4b79f00
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-03-12 21:08:15 +0100

    Actualizar funciones de creación de matriz y coordenadas en el archivo crear_matriz.py y agregar script para resolver el problema del viajante en TSP-OR-tools.py

--- Viajante/Datos/crear_matriz.py
+++ Viajante/Datos/crear_matriz.py
@@ -2,7 +2,7 @@
 import numpy as np
 import random
 
-def crear_matriz(size: int, path: str):
+def crear_matriz_distancias(size: int, path: str):
     """Crea una matriz de distancias entre ciudades"""
 
     matriz = np.zeros((size, size))
@@ -13,5 +13,35 @@ def crear_matriz(size: int, path: str):
     np.savetxt(path, matriz, fmt="%d")
     return matriz
 
+def crear_lista_coordenadas(size: int, path: str, min: float = -100, max: float = 100):
+    """Crea una lista de coordenadas de ciudades en formato (x, y) con valores entre min y max"""
+    coordenadas = []
+    for i in range(size):
+        x = random.uniform(min, max)
+        y = random.uniform(min, max)
+        coordenadas.append((x, y))
+    with open(path, "w") as file:
+        for i in range(size):
+            file.write(f"{coordenadas[i][0]} {coordenadas[i][1]}\n")
+    return coordenadas
+
+def coordenadas_a_distancias(path_coordenadas: str, path_distancias: str):
+    """Lee el fichero con las coordenadas y produce  la matriz de distancias correspondiente"""
+    
+    # Leer las coordenadas
+    with open(path_coordenadas, "r") as file:
+        coordenadas = file.readlines()
+    coordenadas = [tuple(map(float, coord.split())) for coord in coordenadas]
+    size = len(coordenadas)
+    
+    matriz = np.zeros((size, size))
+    
+    for i in range(size):
+        for j in range(i + 1, size):
+            matriz[i][j] = np.sqrt((coordenadas[i][0] - coordenadas[j][0])**2 + (coordenadas[i][1] - coordenadas[j][1])**2)
+            matriz[j][i] = matriz[i][j]
+    np.savetxt(path_distancias, matriz, fmt="%d")
+    
+
 if __name__ == "__main__":
-    crear_matriz(10000, "Viajante/Datos/10k.data")
+    crear_lista_coordenadas(10, "Viajante/Datos/10k.data")
