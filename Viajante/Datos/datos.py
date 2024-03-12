e2a72e3f692de6d61794370c0080ae583a1da9f7
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-03-12 23:22:55 +0100

    Agrega funciones para crear una matriz de distancias entre ciudades y convertir coordenadas en distancias

--- Viajante/Datos/datos.py
+++ Viajante/Datos/datos.py
@@ -1,6 +1,8 @@
 """Crea una matriz de distancias entre ciudades"""
+
 import numpy as np
 import random
+
 
 def crear_matriz_distancias(size: int, path: str):
     """Crea una matriz de distancias entre ciudades"""
@@ -12,6 +14,7 @@ def crear_matriz_distancias(size: int, path: str):
             matriz[j][i] = matriz[i][j]
     np.savetxt(path, matriz, fmt="%d")
     return matriz
+
 
 def crear_lista_coordenadas(size: int, path: str, min: float = -100, max: float = 100):
     """Crea una lista de coordenadas de ciudades en formato (x, y) con valores entre min y max"""
@@ -25,31 +28,54 @@ def crear_lista_coordenadas(size: int, path: str, min: float = -100, max: float 
             file.write(f"{coordenadas[i][0]} {coordenadas[i][1]}\n")
     return coordenadas
 
-def coordenadas_a_distancias(path_coordenadas: str, path_distancias: str, distancia: str = "euclidea", decimales: int = 2):
+
+def coordenadas_a_distancias(
+    path_coordenadas: str,
+    path_distancias: str,
+    distancia: str = "euclidea",
+    decimales: int = 2,
+):
     """Lee el fichero con las coordenadas y produce  la matriz de distancias correspondiente"""
-    
+
     # Leer las coordenadas
     with open(path_coordenadas, "r") as file:
         coordenadas = file.readlines()
     coordenadas = [tuple(map(float, coord.split())) for coord in coordenadas]
     size = len(coordenadas)
-    
+
     matriz = np.zeros((size, size))
-    
+
     for i in range(size):
         for j in range(i + 1, size):
             match distancia:
                 case "euclidea":
-                    matriz[i][j] = np.sqrt((coordenadas[i][0] - coordenadas[j][0])**2 + (coordenadas[i][1] - coordenadas[j][1])**2)
+                    matriz[i][j] = np.sqrt(
+                        (coordenadas[i][0] - coordenadas[j][0]) ** 2
+                        + (coordenadas[i][1] - coordenadas[j][1]) ** 2
+                    )
                 case "manhattan":
-                    matriz[i][j] = abs(coordenadas[i][0] - coordenadas[j][0]) + abs(coordenadas[i][1] - coordenadas[j][1])
+                    matriz[i][j] = abs(coordenadas[i][0] - coordenadas[j][0]) + abs(
+                        coordenadas[i][1] - coordenadas[j][1]
+                    )
                 case "chebyshev":
-                    matriz[i][j] = max(abs(coordenadas[i][0] - coordenadas[j][0]), abs(coordenadas[i][1] - coordenadas[j][1]))
+                    matriz[i][j] = max(
+                        abs(coordenadas[i][0] - coordenadas[j][0]),
+                        abs(coordenadas[i][1] - coordenadas[j][1]),
+                    )
             matriz[j][i] = matriz[i][j]
     np.savetxt(path_distancias, matriz, fmt=f"%.{decimales}f")
-    
+
+
+def leer_coordenadas(path: str):
+    """Lee un fichero con coordenadas y devuelve una lista de tuplas"""
+    with open(path, "r") as file:
+        coordenadas = file.readlines()
+    coordenadas = [tuple(map(float, coord.split())) for coord in coordenadas]
+    return coordenadas
+
 
 if __name__ == "__main__":
     crear_lista_coordenadas(10, "Viajante/Datos/10_coordenadas.txt")
-    coordenadas_a_distancias("Viajante/Datos/10_coordenadas.txt", "Viajante/Datos/10_distancias.txt")
-        
+    coordenadas_a_distancias(
+        "Viajante/Datos/10_coordenadas.txt", "Viajante/Datos/10_distancias.txt"
+    )
